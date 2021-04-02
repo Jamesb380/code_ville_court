5e98ae8b073a97fd154da290f21c4555f75c3901
James B Bennett <jamesbnbennett@gmail.com>
2021-04-02 01:11:32 -0400

    updated requirement doc and deleted unused packages

--- hap.py
+++ hap.py
@@ -6,7 +6,6 @@ import streamlit as st
 import plotly.express as px
 import json
 import streamlit_theme as stt
-from pyspark.sql import SparkSession
 import urllib
 
 ########################## SETTINGS ###################################
